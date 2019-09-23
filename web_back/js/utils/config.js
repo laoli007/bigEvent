8e51f5fb2e270f5eeab90f7b702189dffe25028a
laoli996 <261616719@qq.com>
2019-09-23 16:06:04 +0800

    完成了显示文章分类的功能

---
+++ web_back/js/utils/config.js
@@ -0,0 +1,9 @@
+// 设置url地址
+var userUrl = {
+    login:'http://localhost:8000/admin/login',
+    logout:'http://localhost:8000/admin/logout',
+    getInfo:'http://localhost:8000/admin/getuser',
+}
+var categoryUrl = {
+    category_search:'http://localhost:8000/admin/category_search'
+}
